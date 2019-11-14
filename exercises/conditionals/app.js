bd34b58af0c9919366ba5ef8a1be0e1711d68d26
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2019-11-14 14:15:13 -0700

    compulsive commit 'cuz paranoia

---
+++ exercises/conditionals/app.js
@@ -0,0 +1,22 @@
+
+function checkIfEven(num){
+    num%2===0 ? console.log(num + ": It\'s even") : console.log(num + ": It\'s odd");
+}
+
+
+checkIfEven(7)
+checkIfEven(4)
+
+if (typeof "dog" === 'string')
+{
+    console.log(typeof true + "\n JUST KIDDING, it's a string");
+}
+
+"cat".length === 3 ? console.log("IT'S THREE GUYS") : null;
+
+var person = {
+    name: "Lawrence",
+    status: "Nobleman"
+}
+
+person.name[0].toUpperCase() === "L" ? console.log("welcome to L-Land") : null;
